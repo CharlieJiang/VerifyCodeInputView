cfb0c45862c4603c25f34ff6642bd9bd397c6d44
CharlieJiang <cocoasjiang@foxmail.com>
2020-11-16 15:26:41 +0800

    增加依赖库模块
    将自定义view移到单独的依赖库模块中，方便后续公开供引用

--- app/src/main/java/com/cocoas/verifycodeinputview/MainActivity.java
+++ app/src/main/java/com/cocoas/verifycodeinputview/MainActivity.java
@@ -4,6 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ *  自定义验证码输入框使用示例
+ * @author 蒋庆意
+ * @Date 2020/11/16
+ * @Time 14:38
+ *
+ * By cocoas
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
